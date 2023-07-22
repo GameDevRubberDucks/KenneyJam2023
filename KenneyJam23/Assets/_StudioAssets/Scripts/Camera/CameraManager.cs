b33a6663f2c2b663792fee7efd2cf9e26461f7ef
Bo <rubberducks.byu@gmail.com>
2023-07-21 23:46:34 -0400

    Merged a few things

--- KenneyJam23/Assets/_StudioAssets/Scripts/Camera/CameraManager.cs
+++ KenneyJam23/Assets/_StudioAssets/Scripts/Camera/CameraManager.cs
@@ -8,49 +8,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-
-public class CameraManager : MonoBehaviour
+namespace RubberDucks.KenneyJam.Camera
 {
-
-    public CinemachineTargetGroup targetGroup;
-    public float breakAwayDistance;
-
-    private Vector3 groupCenter;
-    private float[] targetDistanceFromCenter;
-    private int[] targetWeight;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CameraManager : MonoBehaviour
     {
-        targetWeight = new int[targetGroup.m_Targets.Length];
-        targetDistanceFromCenter = new float[targetGroup.m_Targets.Length];
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        DetchTargetFromGroup();
-    }
+        public CinemachineTargetGroup targetGroup;
+        public float breakAwayDistance;
 
-    void DetchTargetFromGroup()
-    {
-        //store location of the center of the group
-        groupCenter = targetGroup.transform.position;
+        private Vector3 groupCenter;
+        private float[] targetDistanceFromCenter;
+        private int[] targetWeight;
 
-        for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+        // Start is called before the first frame update
+        void Start()
         {
-            targetDistanceFromCenter[i] = Vector3.Distance(groupCenter, targetGroup.m_Targets[i].target.position);
-
-            //NOTE: there should be a way to have this not setting the weights every frame
-            if (targetDistanceFromCenter[i] > breakAwayDistance)
-            {
-                targetGroup.m_Targets[i].weight = 0;
-            }
-            else
-            {
-                targetGroup.m_Targets[i].weight = 1;
-            }
+            targetWeight = new int[targetGroup.m_Targets.Length];
+            targetDistanceFromCenter = new float[targetGroup.m_Targets.Length];
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            DetchTargetFromGroup();
+        }
+
+        void DetchTargetFromGroup()
+        {
+            //store location of the center of the group
+            groupCenter = targetGroup.transform.position;
+
+            for (int i = 0; i < targetGroup.m_Targets.Length; i++)
+            {
+                targetDistanceFromCenter[i] = Vector3.Distance(groupCenter, targetGroup.m_Targets[i].target.position);
+
+                //NOTE: there should be a way to have this not setting the weights every frame
+                if (targetDistanceFromCenter[i] > breakAwayDistance)
+                {
+                    targetGroup.m_Targets[i].weight = 0;
+                }
+                else
+                {
+                    targetGroup.m_Targets[i].weight = 1;
+                }
+            }
+
+        }
     }
 }
